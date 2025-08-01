d843926b2b06a0455a1049641a604baed66497fb
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-01 09:16:28 +0200

    Create BlogController and implement blog listing on Index view

---
+++ TravelTripProject/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Data;
+
+namespace TravelTripProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var blogs = context.Blogs.ToList();
+            return View(blogs);
+        }
+    }
+}
