5798b6ad5236e45663efacb4604e4c89a2c4f1c0
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-01 18:49:53 +0200

    Display blog images dynamically on homepage using data from database

--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProject.Data;
 
 namespace TravelTripProject.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        Context context = new Context();
         public ActionResult Index()
         {
-            return View();
+            var values = context.Blogs.ToList();
+            return View(values);
         }
 
         public ActionResult About()
