bba38cf9114b2e0b33ca4bfe235e7131729ef1d0
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-03 23:38:20 +0200

    Added "Add New Blog" view for admin panel with Bootstrap form styling
    - Created AddBlog.cshtml under Admin views.
    - Implemented form fields for Title, Description, Image URL, and Date.
    - Included Submit and Cancel buttons with clean Bootstrap design.
    - Integrated with AdminLayout for consistent UI.

--- TravelTripProject/Controllers/AdminController.cs
+++ TravelTripProject/Controllers/AdminController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProject.Data;
+using TravelTripProject.Models;
+
 
 namespace TravelTripProject.Controllers
 {
     public class AdminController : Controller
     {
+        Context context = new Context();
         // GET: Admin
         public ActionResult Index()
         {
+            var values = context.Blogs.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddBlog()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddBlog(Blog blog)
+        {
+            context.Blogs.Add(blog);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
